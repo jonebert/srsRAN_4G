a038e5d1995cd87d970e901d269a160d659b002f
Jonathan Ebert <jonathan.ebert18@gmail.com>
2024-07-15 14:38:12 +0200

    fix

---
+++ srsue/hdr/common.h
@@ -0,0 +1,11 @@
+#ifndef SRSUE_COMMON_H
+#define SRSUE_COMMON_H
+
+#include <atomic>
+
+namespace srsue {
+
+extern std::atomic<bool> request_performed;
+
+} // namespace srsue
+#endif // SRSUE_COMMON_H
