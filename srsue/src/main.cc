2cd6876d2dd090f38a4b2786cc091674ba927bce
Jonathan Ebert <jonathan.ebert18@gmail.com>
2024-07-29 12:14:11 +0200

    fix

--- srsue/src/main.cc
+++ srsue/src/main.cc
@@ -816,20 +816,21 @@ int main(int argc, char* argv[])
   unsigned performed_requests = 0;
   signal(SIGUSR1, restart_signal_handler);
 
+  srsue::ue ue;
+  if (ue.init(args)) {
+    ue.stop();
+    return SRSRAN_SUCCESS;
+  }
+
   while (running) {
     request_performed = false;
-    srsue::ue ue;
-    if (ue.init(args)) {
-      ue.stop();
-      return SRSRAN_SUCCESS;
-    }
     cout << "Attaching UE..." << endl;
     ue.switch_on();
 
     while (!request_performed) {
       std::this_thread::sleep_for(chrono::milliseconds(100));
     }
-    // ue.stop();
+    ue.stop();
 
     performed_requests++;
     cout << "Performed Requests: " << performed_requests << endl;
